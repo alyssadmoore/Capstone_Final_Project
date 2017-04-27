448d4cffb624d6a0c08492918d62a8ae142e13c5
alyssadmoore <alyssadmoore@gmail.com>
2017-04-27 14:00:37 -0500

    Add draw_x function

--- main.py
+++ main.py
@@ -6,12 +6,11 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+GRAY = (205, 201, 201)
 
-# Set width and height of each cell, in pixels
+# Set width and height of each cell, as well as margins between cells, in pixels
 WIDTH = 20
 HEIGHT = 20
-
-# Set margin, in pixels, between each cell
 MARGIN = 2
 
 # Create a 2 dimensional array to hold information on each cell
@@ -22,10 +21,12 @@ for row in range(10):
     for column in range(10):
         grid[row].append(0)
 
-# Example to change cell information
-grid[2][6] = 1  # 3 blocks down, 7 blocks over
-grid[6][2] = 2  # 7 blocks down, 3 blocks over
-grid[0][0] = 3  # Upper left corner
+# Possible values in above array are mapped as follows:
+# 0 = unclicked
+# 1-8 = num bombs around clicked cell
+# 9 = clicked, clear
+# 10 = flag
+# 11 = bomb
 
 # Initialize pygame
 pygame.init()
@@ -37,57 +38,106 @@ screen = pygame.display.set_mode(WINDOW_SIZE)
 # Set title of screen
 pygame.display.set_caption("Minesweeper")
 
-# Used to loop until the user clicks the close button
-done = False
-
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
 
+
+# Draws an X to represent a flag placed on a tile
+def draw_x(row, column):
+    upper = (MARGIN + WIDTH) * column + MARGIN
+    left = (MARGIN + HEIGHT) * row + MARGIN
+    lower = upper + WIDTH
+    right = left + HEIGHT
+    # Test drawing a red X
+    # (screen, color, closed, [uppermost point, leftmost point, width, height], thickness)
+    pygame.draw.line(screen, RED, (upper, left), (lower, right), 2)
+    pygame.draw.line(screen, RED, (upper, right), (lower, left), 2)
+
+
+# Returns number of bombs around clicked cell
+def check_num_bombs(row, column):
+    check1 = grid[row-1][column-1]
+    check2 = grid[row][column-1]
+    check3 = grid[row+1][column-1]
+    check4 = grid[row][column+1]
+    check5 = grid[row-1][column]
+    check6 = grid[row+1][column]
+    check7 = grid[row-1][column+1]
+    check8 = grid[row+1][column+1]
+    checklist = [check1, check2, check3, check4, check5, check6, check7, check8]
+
+    counter = 0
+    for x in checklist:
+        if x == 11:
+            counter += 1
+
+    return counter
+
+
 # Main loop
-while not done:
-    # An event means the user did something
-    for event in pygame.event.get():
-        # If the user clicked the close button, use flag to exit loop
-        if event.type == pygame.QUIT:
-            done = True
-        # If the user clicked somewhere, we need to translate the mouse position to the corresponding cell
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            # Get mouse position
-            pos = pygame.mouse.get_pos()
-            # Convert x/y screen coordinates to grid coordinates
-            column = pos[0] // (WIDTH + MARGIN)
-            row = pos[1] // (HEIGHT + MARGIN)
-            # Set that location to one (changes color to green, for testing)
-            grid[row][column] = 1
+def main():
+    # Used to loop until the user clicks the close button
+    done = False
+    while not done:
+        # An event means the user did something
+        for event in pygame.event.get():
+            # If the user clicked the close button, use flag to exit loop
+            if event.type == pygame.QUIT:
+                done = True
+            # Mousedown can mean left, middle, or right button: left is 1, middle is 2, right is 3
+            # Left button digs, right button places a flag
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    pos = pygame.mouse.get_pos()    # Get mouse position
+                    column = pos[0] // (WIDTH + MARGIN)     # Convert x/y screen coordinates to cell coordinates
+                    row = pos[1] // (HEIGHT + MARGIN)
 
-    # Set the screen background
-    screen.fill(BLACK)
+                    # TODO "You Lose" screen
+                    # If the tile is a bomb, player loses
+                    if grid[row][column] == 11:
+                        print("You lose")
 
-    # Draws the grid on top of the background
-    for row in range(10):
-        for column in range(10):
-            # "empty" cells are colored white (value 0)
-            color = WHITE
-            # Set some initial colors for testing
-            if grid[row][column] == 1:
-                color = GREEN
-            elif grid[row][column] == 2:
-                color = RED
-            elif grid[row][column] == 3:
-                color = BLUE
-            # Cells actually created here
-            pygame.draw.rect(screen,
-                             color,
-                             [(MARGIN + WIDTH) * column + MARGIN,
-                              (MARGIN + HEIGHT) * row + MARGIN,
-                              WIDTH,
-                              HEIGHT])
+                    # If we get this far, player has NOT clicked a bomb- check for bombs around clicked tile
+                    bombs = check_num_bombs(row, column)
+                    grid[row][column] = 1   # No matter what, color tile white
+                    # TODO draw 1-8 functions
 
-    # Limit refresh rate to 60 frames per second
-    clock.tick(60)
+                elif event.button == 3:
+                    pos = pygame.mouse.get_pos()
+                    column = pos[0] // (WIDTH + MARGIN)
+                    row = pos[1] // (HEIGHT + MARGIN)
+                    grid[row][column] = 9
 
-    # Update the screen
-    pygame.display.flip()
+        # Set the screen background
+        screen.fill(BLACK)
 
-# Once we have closed the window, this stops the process
-pygame.quit()
+        # Draws the grid on top of the background
+        for row in range(10):
+            for column in range(10):
+                # Unclicked cells are gray (default)
+                color = GRAY
+                # Clicked but empty cells are white
+                if grid[row][column] == 1:
+                    color = WHITE
+                # Cells actually created here (screen, color, [uppermost point, leftmost point, width, height])
+                pygame.draw.rect(screen,
+                                 color,
+                                 [(MARGIN + WIDTH) * column + MARGIN,
+                                  (MARGIN + HEIGHT) * row + MARGIN,
+                                  WIDTH,
+                                  HEIGHT])
+                # So whole map isn't drawn over X's
+                if grid[row][column] == 9:
+                    draw_x(row, column)
+
+        # Limit refresh rate to 60 frames per second
+        clock.tick(60)
+
+        # Update the screen
+        pygame.display.flip()
+
+    # Once we have closed the window, this stops the process
+    pygame.quit()
+
+
+main()
